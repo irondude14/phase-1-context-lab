fc8edba2d1deb85868fc1e1288e224087261abaf
Vladyslav <vkadyrov@protonmail.com>
2022-10-18 18:46:57 -0400

    commit tree 58c43db03cf759bcc6cbcaf5f9ba69f943a0c785

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    const employeeRecord = {};
+    employeeRecord.firstName = array[0];
+    employeeRecord.familyName = array[1];
+    employeeRecord.title = array[2];
+    employeeRecord.payPerHour = array[3];
+    employeeRecord.timeInEvents = [];
+    employeeRecord.timeOutEvents = [];
+    return employeeRecord;
+}
+
+function createEmployeeRecords(array) {
+    let records = array.map(record => {
+            return createEmployeeRecord(record);
+        })
+    return records
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(time, 10),
+    date: date,
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time, 10),
+        date: date,
+        });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const hoursIn = () => {
+        for (const timeEntreeIn of this.timeInEvents) {
+                if (date === timeEntreeIn.date) {
+                    return timeEntreeIn.hour
+                }
+            }   
+        }
+    const hoursOut = () => {
+        for (const timeEntreeOut of this.timeOutEvents) {
+                    if (date === timeEntreeOut.date) {
+                        return timeEntreeOut.hour
+                }
+            } 
+        }
+    let hoursWorked = (hoursOut() - hoursIn());
+    let numOfHours = hoursWorked.toString().substring(0, hoursWorked.toString().length - 2);
+    return parseInt(numOfHours)           
+}
+
+
+function wagesEarnedOnDate(date) {
+    // let hoursWorked = hoursWorkedOnDate(date);
+    let pay = this.payPerHour;
+    let wages = pay * hoursWorkedOnDate.call(this, date);
+    return wages;
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +87,20 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (const name of srcArray) {
+            if (name.firstName === firstName) {
+                return name
+            }
+    }
+    return undefined
+}
+
+function calculatePayroll(array) {
+    let allWages = []
+    for (const e of array) {
+        allWages.push(allWagesFor.call(e))
+    }
+    let total = allWages.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    return total
+}
